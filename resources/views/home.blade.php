88431af0e9d6ea64bf9b4f1363e0a3692f4aa3d4
Ajitama38 <anjitama38@gmail.com>
2023-09-22 14:23:23 +0700

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
+@extends('css.style')
 @extends('layouts.main')
 
 @section('container')
-
-<h1>Halaman Home</h1>
-
-
+<section class="hero" id="homeku">
+    <main class="content">
+        <h1>Rental Mobil Andalan Keluarga</h1>
+        <p>Mobilku Mobilmu, Mobil kita semua</p>
+        <a href="Akun/formulir/formulir_Sewa" class="cta">Sewa Sekarang</a>
+    </main>
+</section>
 @endsection
-<style>
-    background-image: url(/image/bg.jpg);
-</style>
