b0fbbe046a758eba08ac46c66257fb80a752b7ba
Ajitama38 <anjitama38@gmail.com>
2023-09-22 13:09:17 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/sewa', function () {
+    return view('sewa', [
+        "title" => "Sewa"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About"
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', [
+        "title" => "Contact"
+    ]);
+});
