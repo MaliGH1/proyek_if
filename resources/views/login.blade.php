4d00b233fd661b1086eef4d27ff1a13656bb5268
alexadma <alexadma16@gmail.com>
2023-09-22 16:54:58 +0700

    Commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menu Login</title>
+    <link rel="stylesheet" type="text/css" href="LoginCss/login.css">
+    
+</head>
+<body>
+
+<div class="login-container">
+    <h2>Rental Mobil</h2>
+    <div class="login-form">
+        <input type="text" placeholder="Username">
+        <input type="password" placeholder="Password">
+        <button>Login</button>
+    </div>
+</div>
+
+</body>
+</html>
