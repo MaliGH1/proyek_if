4bcc5188742374dfbfc8a562029bb55c37176425
Ajitama38 <anjitama38@gmail.com>
2023-09-28 19:11:06 +0700

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,10 @@
 <section class="hero" id="home">
     <main class="content">
         <h1>Rental Mobil Andalan Keluarga</h1>
-        <p>Mobilku Mobilmu, Mobil kita semua</p>
+        <article>
+            Mobilku Mobilmu, Mobil kita semua
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum adipisci eos est a labore at atque perspiciatis. Fugiat, ad reprehenderit minus sequi sapiente repudiandae nihil vero cumque itaque, impedit quidem.
+        </article>
     </main>
 </section>
 @endsection
