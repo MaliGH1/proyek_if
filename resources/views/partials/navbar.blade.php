44e4fddbfbc8055d30ee7d4dfb2c78cfbb4c1f9b
Ajitama38 <anjitama38@gmail.com>
2023-09-29 11:50:50 +0700

    commit

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,26 +1,28 @@
-<nav class="navbar navbar-light navbar-expand-lg bg-light">
-    <div class="container navbar-center">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse justify-content-center navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-2 mb-3 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" href="/home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ ($title === "Sewa") ? 'active' : '' }}" href="/sewa">Sewa</a>
-                </li>
-                <li class="nav-item">
-                    <a class="navbar-brand" href="/home">DVJR</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ ($title === "Contact") ? 'active' : '' }}" href="/contact">Contact</a>
-                </li>
-            </ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="bg-white p-6">
+        <div class="container mx-auto">
+            <div class="flex items-center justify-center ">
+                <div class="space-x-4 flex gap-5">
+                    <a class="text-black no-underline" href="/home">Home</a>
+                    <a class="text-black no-underline" href="/sewa">Sewa</a>
+                    <div class="px-56">
+                        <a class="text-black no-underline text-2xl font-semibold" href="/home">DVJR</a>
+                    </div>
+                    <a class="text-black no-underline" href="/about">About</a>
+                    <a class="text-black no-underline" href="/contact">Contact</a>
+                </div>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</body>
+
+</html>
