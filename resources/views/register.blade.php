d74ef1d0a6165493ccb07a2dd9487edff4d25e8e
BasiliusDimas019 <basiliusarilla@gmail.com>
2023-10-04 15:18:51 +0700

    commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,15 +8,30 @@
 
 <body>
 <form action="/register" method="post">
+@csrf
 
     <div class="registration-container">
         <h2>Registrasi Akun</h2>
         <div class="registration-form">
-            <input type="text" id="username" placeholder="USERNAME" required>
-            <input type="text" id="fullname" placeholder="NAMA LENGKAP" required>
-            <input type="tel" id="phone" placeholder="NO. HP" required>
-            <input type="password" id="password" placeholder="PASSWORD" required>
-            <input type="password" id="confirm-password" placeholder="CONFRIM PASSWORD" required>
+
+            <label for="fullname">Nama Lengkap:</label>
+            <input type="text" id="fullname" name="fullname" placeholder="Masukkan nama lengkap">
+            
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" placeholder="Masukkan username">
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Masukkan email">
+
+            <label for="phone">No. HP:</label>
+            <input type="tel" id="phone" name="phone" placeholder="Masukkan nomor HP">
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Masukkan password">
+
+            <label for="confirm-password">Konfirmasi Password:</labels>
+                <input type="password" id="confirm-password" name="confirm-password" placeholder="Konfirmasi password">
+
                 <a href="/login"><button class="signup-button">BUAT AKUN</button></a>
         </div>
     </div>
