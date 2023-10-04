d868cea7ff2ed86d1c212e594ca3cab1a23b0f61
alexadma <alexadma16@gmail.com>
2023-10-04 15:32:51 +0700

    commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,39 +3,39 @@
 
 <head>
     <title>Registrasi Akun</title>
-    <link rel="stylesheet" type="text/css" href="register.css">
+    <link rel="stylesheet" type="text/css" href="css/register.css">
 </head>
 
 <body>
-<form action="/register" method="post">
-@csrf
+    <form action="/register" method="post">
+        @csrf
 
-    <div class="registration-container">
-        <h2>Registrasi Akun</h2>
-        <div class="registration-form">
+        <div class="registration-container">
+            <h2>Registrasi Akun</h2>
+            <div class="registration-form">
 
-            <label for="fullname">Nama Lengkap:</label>
-            <input type="text" id="fullname" name="fullname" placeholder="Masukkan nama lengkap">
-            
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="Masukkan username">
+                <label for="fullname">Nama Lengkap:</label>
+                <input type="text" id="fullname" name="fullname" placeholder="Masukkan nama lengkap">
 
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Masukkan email">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" placeholder="Masukkan username">
 
-            <label for="phone">No. HP:</label>
-            <input type="tel" id="phone" name="phone" placeholder="Masukkan nomor HP">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Masukkan email">
 
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Masukkan password">
+                <label for="phone">No. HP:</label>
+                <input type="tel" id="phone" name="phone" placeholder="Masukkan nomor HP">
 
-            <label for="confirm-password">Konfirmasi Password:</labels>
-                <input type="password" id="confirm-password" name="confirm-password" placeholder="Konfirmasi password">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" placeholder="Masukkan password">
 
-                <a href="/login"><button class="signup-button">BUAT AKUN</button></a>
+                <label for="confirm-password">Konfirmasi Password:</labels>
+                    <input type="password" id="confirm-password" name="confirm-password" placeholder="Konfirmasi password">
+
+                    <a href="/login"><button class="signup-button">BUAT AKUN</button></a>
+            </div>
         </div>
-    </div>
-</form>
+    </form>
 
 </body>
 
