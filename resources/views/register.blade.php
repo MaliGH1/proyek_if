361dcb4293c5a2ac696326c548fcdffc46156eee
BasiliusDimas019 <basiliusarilla@gmail.com>
2023-10-04 15:44:31 +0700

    commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -15,23 +15,49 @@
         <div class="registration-form">
 
             <label for="fullname">Nama Lengkap:</label>
-            <input type="text" id="fullname" name="fullname" placeholder="Masukkan nama lengkap">
-            
+            <input type="text" id="fullname" name="fullname" class="@error('fullname') is-invalid @enderror" placeholder="Masukkan nama lengkap">
+            @error('fullname')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+
+            <br>
             <label for="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="Masukkan username">
+            <input type="text" id="username" name="username" class="@error('username') is-invalid @enderror" placeholder="Masukkan username">
+            @error('username')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
 
+            <br>
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Masukkan email">
+            <input type="email" id="email" name="email" class="@error('email') is-invalid @enderror" placeholder="Masukkan email">
+            @error('email')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
 
+            <br>
             <label for="phone">No. HP:</label>
-            <input type="tel" id="phone" name="phone" placeholder="Masukkan nomor HP">
+            <input type="tel" id="phone" name="phone" class="@error('phone') is-invalid @enderror" placeholder="Masukkan nomor HP">
+            @error('phone')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
 
+            <br>
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Masukkan password">
-
-            <label for="confirm-password">Konfirmasi Password:</labels>
-                <input type="password" id="confirm-password" name="confirm-password" placeholder="Konfirmasi password">
-
+            <input type="password" id="password" name="password" class="@error('password') is-invalid @enderror" placeholder="Masukkan password">
+            @error('password')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+            
                 <a href="/login"><button class="signup-button">BUAT AKUN</button></a>
         </div>
     </div>
