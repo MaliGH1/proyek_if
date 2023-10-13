629fbc3559dfc4db88e40ac4690352e8adfb3093
Ajitama38 <anjitama38@gmail.com>
2023-10-13 11:30:44 +0700

    commit

--- resources/views/partials/navbaradmin.blade.php
+++ resources/views/partials/navbaradmin.blade.php
@@ -17,42 +17,67 @@
         <div class="text-gray-100 text-xl">
             <div class="p-2.5 mt-1 flex items-center">
                 <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
-                <h1 class="font-bold text-gray-200 text-[15px] ml-3">TailwindCSS</h1>
+                <h1 class="font-bold text-gray-200 text-[15px] ml-3">DVJR</h1>
                 <i class="bi bi-x cursor-pointer ml-28 lg:hidden" onclick="openSidebar()"></i>
             </div>
             <div class="my-2 bg-gray-600 h-[1px]"></div>
-        </div>
-        <div class="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white">
-            <i class="bi bi-search text-sm"></i>
-            <input type="text" placeholder="Search" class="text-[15px] ml-4 w-full bg-transparent focus:outline-none" />
         </div>
         <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
             <i class="bi bi-house-door-fill"></i>
             <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
         </div>
-        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-            <i class="bi bi-bookmark-fill"></i>
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">Bookmark</span>
-        </div>
-        <div class="my-4 bg-gray-600 h-[1px]"></div>
-        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white" onclick="dropdown()">
+        <div class="my-2 bg-gray-600 h-[1px]"></div>
+        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer" onclick="dropdown('sewa')">
             <i class="bi bi-chat-left-text-fill"></i>
             <div class="flex justify-between w-full items-center">
-                <span class="text-[15px] ml-4 text-gray-200 font-bold">Chatbox</span>
-                <span class="text-sm rotate-180" id="arrow">
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">Sewa</span>
+                <span class="text-sm rotate-180" id="arrow-sewa">
                     <i class="bi bi-chevron-down"></i>
                 </span>
             </div>
         </div>
-        <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
+        <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu-sewa">
             <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                Social
+                Verifikasi
             </h1>
-            <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                Personal
+            <h1 class="cursor-pointer p-2 hover-bg-blue-600 rounded-md mt-1">
+                Pengembalian
             </h1>
-            <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                Friends
+        </div>
+        <div class="my-2 bg-gray-600 h-[1px]"></div>
+        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer" onclick="dropdown('edit')">
+            <i class="bi bi-chat-left-text-fill"></i>
+            <div class="flex justify-between w-full items-center">
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">Edit</span>
+                <span class="text-sm rotate-180" id="arrow-edit">
+                    <i class="bi bi-chevron-down"></i>
+                </span>
+            </div>
+        </div>
+        <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu-edit">
+            <h1 class="cursor-pointer p-2 hover-bg-blue-600 rounded-md mt-1">
+                Supir
+            </h1>
+            <h1 class="cursor-pointer p-2 hover-bg-blue-600 rounded-md mt-1">
+                Mobil
+            </h1>
+        </div>
+        <div class="my-2 bg-gray-600 h-[1px]"></div>
+        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer" onclick="dropdown('keuangan')">
+            <i class="bi bi-chat-left-text-fill"></i>
+            <div class="flex justify-between w-full items-center">
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">Keuangan</span>
+                <span class="text-sm rotate-180" id="arrow-keuangan">
+                    <i class="bi bi-chevron-down"></i>
+                </span>
+            </div>
+        </div>
+        <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu-keuangan">
+            <h1 class="cursor-pointer p-2 hover-bg-blue-600 rounded-md mt-1">
+                Bulanan
+            </h1>
+            <h1 class="cursor-pointer p-2 hover-bg-blue-600 rounded-md mt-1">
+                Tahunan
             </h1>
         </div>
         <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
@@ -62,11 +87,12 @@
     </div>
 
     <script type="text/javascript">
-        function dropdown() {
-            document.querySelector("#submenu").classList.toggle("hidden");
-            document.querySelector("#arrow").classList.toggle("rotate-0");
+        function dropdown(menu) {
+            const submenu = document.querySelector(`#submenu-${menu}`);
+            const arrow = document.querySelector(`#arrow-${menu}`);
+            submenu.classList.toggle("hidden");
+            arrow.classList.toggle("rotate-180");
         }
-        dropdown();
 
         function openSidebar() {
             document.querySelector(".sidebar").classList.toggle("hidden");
