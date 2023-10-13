b72069d79cf9f3aac055f39e07a75c16accb118c
Ajitama38 <anjitama38@gmail.com>
2023-10-13 15:17:47 +0700

    commit

--- resources/views/customer/sewa.blade.php
+++ resources/views/customer/sewa.blade.php
@@ -1,21 +1,22 @@
+<link rel="stylesheet" href="/css/sewa.css" class="">
 @extends('layouts.mainsewa')
 
 @section('container')
 <div class="container mx-auto mt-8">
-    <div class="bg-white p-4 rounded-lg shadow-md">
-        <h1 class="text-2xl font-semibold mb-4">Formulir Pengisian Penyewaan Mobil</h1>
+    <div class="layout">
+        <h1 class="judul">Formulir Pengisian Penyewaan Mobil</h1>
 
         <form action="/sewa" method="POST">
             @csrf
-            <div class="mb-4 flex items-center">
-                <label for="nama" class="text-sm font-medium text-gray-700 mr-2" style="max-width: 150px; overflow: hidden; white-space: nowrap;">Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                <input type="text" id="nama" name="nama" class="w-75 px-4 py-2 border rounded-lg">
+            <div class="nama">
+                <label for="nama" class="nama-label">Nama</label>
+                <input type="text" id="nama" name="nama" class="nama-input">
             </div>
 
-            <div class="mb-4 flex items-center">
-                <label for="pilih" class="text-sm font-medium text-gray-700 mr-2" style="max-width: 150px; overflow: hidden; white-space: nowrap;">Pilih Mobil:</label>
+            <div class="mobil">
+                <label for="pilih" class="mobil-label">Pilih Mobil</label>
                 <div class="w-full">
-                    <select id="pilih" name="pilih" class="w-75 px-4 py-2 border rounded-lg">
+                    <select id="pilih" name="pilih" class="mobil-input">
                         <option value="mobil1">Mazda Rx-7</option>
                         <option value="mobil2">Toyota Alphard</option>
                         <option value="mobil3">Toyota Yaris</option>
@@ -24,8 +25,8 @@
             </div>
 
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700">Apakah Anda Ingin Menyewa Supir?</label>
+            <div class="supir">
+                <label class="supir-label">Apakah Anda Ingin Menyewa Supir?</label>
                 <div class="mt-2">
                     <label class="inline-flex items-center">
                         <input type="radio" name="supir" value="ya" class="form-radio">
@@ -38,17 +39,17 @@
                 </div>
             </div>
 
-            <div class="mb-4">
-                <label for="tanggal-sewa" class="block text-sm font-medium text-gray-700" style="max-width: 150px; overflow: hidden; white-space: nowrap;">Tanggal Sewa:</label>
-                <input type="date" id="tanggal-sewa" name="tanggal_sewa" class="w-75 px-4 py-2 border rounded-lg">
+            <div class="tanggalsewa">
+                <label for="tanggal-sewa" class="tanggalsewa-label">Tanggal Sewa:</label>
+                <input type="date" id="tanggal-sewa" name="tanggal_sewa" class="tanggalsewa-input">
             </div>
 
-            <div class="mb-4">
-                <label for="tanggal-kembali" class="block text-sm font-medium text-gray-700" style="max-width: 150px; overflow: hidden; white-space: nowrap;">Tanggal Kembali:</label>
-                <input type="date" id="tanggal-kembali" name="kembali" class="w-75 px-4 py-2 border rounded-lg">
+            <div class="tanggalkembali">
+                <label for="tanggal-kembali" class="tanggalkembali-label">Tanggal Kembali:</label>
+                <input type="date" id="tanggal-kembali" name="kembali" class="tanggalkembali-input">
             </div>
 
-            <div class="mb-4 flex items-center">
+            <div class="buktitf">
                 <label for="bukti-tf" class="text-sm font-medium text-gray-700 mr-2" style="max-width: 150px; overflow: hidden; white-space: nowrap;">Upload Bukti Transfer:</label>
                 <input type="file" id="bukti-tf" name="bukti_tf" class="w-75 px-4 py-2 border rounded-r-lg">
             </div>
@@ -57,7 +58,7 @@
             <form method="POST" action="/proses-form">
                 @csrf
                 <!-- Input lainnya -->
-                <div class="mb-4">
+                <div class="totalbiaya">
                     <label for="total-biaya" class="block text-sm font-medium text-gray-700">Total Biaya:</label>
                     <input type="text" id="total-biaya" name="total_biaya" class="w-full px-4 py-2 border rounded-lg" value="{{ $transaksi->total_biaya ?? '' }}">
                 </div>
@@ -65,7 +66,7 @@
             </form>
 
 
-            <div class="mt-4">
+            <div class="btnsewa">
                 <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-lg">Sewa Mobil</button>
             </div>
         </form>
