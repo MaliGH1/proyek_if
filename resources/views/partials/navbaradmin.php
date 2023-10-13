f9c36d48e319f786cc9e3fb181f2b4732a34988a
Ajitama38 <anjitama38@gmail.com>
2023-10-13 10:39:26 +0700

    commit

---
+++ resources/views/partials/navbaradmin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Home</title>
+    <!-- Tambahkan link CSS Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-200">
+    <div class="flex h-screen">
+        <div class="bg-gray-800 text-white p-4">
+            <ul>
+                <li class="mb-2"><a href="#" class="block p-2 hover:bg-gray-600">Dashboard</a></li>
+                <li class="mb-2"><a href="#" class="block p-2 hover:bg-gray-600">Verification</a></li>
+                <li class="mb-2"><a href="#" class="block p-2 hover:bg-gray-600">Cars</a></li>
+                <li class="mb-2"><a href="#" class="block p-2 hover:bg-gray-600">Drivers</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
