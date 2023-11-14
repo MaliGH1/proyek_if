44a7f192787496a4f9b785d740e271f155e5ecc3
Jafarrr25 <fajaralnajiim09@gmail.com>
2023-11-14 22:58:16 +0700

    commit

--- resources/views/partials/navbaradmin.blade.php
+++ resources/views/partials/navbaradmin.blade.php
@@ -87,8 +87,13 @@
             </h1>
         </div>
         <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-            <i class="bi bi-box-arrow-in-right"></i>
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white bg-transparent border-none">
+                <i class="bi bi-box-arrow-in-right"></i>
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+            </button>
+        </form>
         </div>
     </div>
 
