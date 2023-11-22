b787f2a16594189eb48ebe75873790423293ed22
Ajitama38 <anjitama38@gmail.com>
2023-11-22 16:30:12 +0700

    commit

--- app/Models/Supir.php
+++ app/Models/Supir.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,11 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Supir extends Model
 {
     use HasFactory;
-
-    protected $primaryKey = 'noktp';
-
-    protected $fillable = [
-        'noktp','nama', 'alamat', 'nohpsupir', 'foto'
-    ];
-    
 }
