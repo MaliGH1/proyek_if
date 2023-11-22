1cfa4555e5a9ccc4ddbbda347f949d9b3d047e92
MaliGH1 <rizkyfadjarmali@gmail.com>
2023-11-22 16:05:04 +0700

    commit

--- resources/views/customer/sewa.blade.php
+++ resources/views/customer/sewa.blade.php
@@ -90,9 +90,9 @@
                                 </h5>
 
                                 <p class="mb-3 font-normal text-black-700 dark:text-black-400">
-                                    <strong style="font-size: 1.2em;">Rp. 300.000/hari</strong>
+                                    <strong style="font-size: 1.2em;">Rp. 500.000/hari</strong>
                                 </p>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Toyota Yaris</p>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Mitsubishi Pajero</p>
                                 <a href="#"
                                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Sewa
