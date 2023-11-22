9871a342a1133e4de1970445512e18c341bc9b06
Ajitama38 <anjitama38@gmail.com>
2023-11-22 20:46:30 +0700

    commit

--- resources/views/mobil/hapusmobil.blade.php
+++ resources/views/mobil/hapusmobil.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.mainadmin')
 
 @section('container')
-<section class="hero" id="home">
-    <div class="container flex left bg-black">
+<section class="">
+    <div class="flex flex-warp">
         <form class="w-full max-w-lg">
-            <div class="flex flex-wrap -mx-3 mb-6">
-                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            <div class="m-6">
+                <div class="">
+                    <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-first-name">
                         Nomor Polisi
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text">
                     <p class="text-red-500 text-xs italic">Please fill out this field.</p>
                 </div>
             </div>
-
         </form>
-        <div class="">
-            <a href="" class="block no-underline">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                    Simpan
-                </button>
-            </a>
-        </div>
+    </div>
+    <div class="m-6">
+        <a href="" class="block no-underline">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                Simpan
+            </button>
+        </a>
+    </div>
     </div>
 </section>
 @endsection
--- resources/views/supir/hapussupir.blade.php
+++ resources/views/supir/hapussupir.blade.php
@@ -1,31 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.mainadmin')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form class="w-full max-w-lg">
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                    ID Supir
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
-                <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+@section('container')
+<section class="">
+    <div class="flex flex-wrap">
+        <form class="w-full max-w-lg">
+            <div class=" m-6">
+                <div class="">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                        ID Supir
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text">
+                    <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                </div>
             </div>
-        </div>
-        <div class="">
-            <a href="" class="block no underline">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                    Hapus
-                </button>
-            </a>
-        </div>
-    </form>
-</body>
+            <div class="m-6">
+                <a href="" class="block no underline">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                        Hapus
+                    </button>
+                </a>
+            </div>
+        </form>
+    </div>
 
-</html>
+</section>
+@endsection
