f2322ff295f60c083d6a4cb6fc22dac99cca5d51
Ajitama38 <anjitama38@gmail.com>
2023-11-22 21:03:40 +0700

    commit

--- resources/views/daftarmobil.blade.php
+++ resources/views/daftarmobil.blade.php
@@ -1,35 +1,29 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DVJR Rent Cars | {{ $title }}</title>
-    <link rel="stylesheet" href="css/main.css" class="">
-    <link href="/dist/tailwind.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    @vite('resources/css/app.css')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-
-<body>
-    @foreach( $mobil as $mobil)
-    <div class="container">
-        <div class="flex flex-wrap max-w-sm rounded overflow-hidden shadow-lg">
-            <img src="{{ $mobil->foto }}" alt="mobil" class="">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">{{ $mobil->nama_mobil }}</div>
-                <p class="text-gray-700 text-base">{{ $mobil->nopol }}</p>
-                <p class="text-gray-700 text-base">{{ $mobil->type }}</p>
-                <p class="text-gray-700 text-base">{{ $mobil->warna }}</p>
-                <p class="text-gray-700 text-base">{{ $mobil->sewa }}</p>
-                <p class="text-gray-700 text-base">{{ $mobil->status }}</p>
-            </div>
+@section('container')
+<section class="pt-20">
+    <div class="flex flex-wrap">
+        @foreach( $mobil as $mobil )
+        <div class="bg-white">
+            <a href="/sewa" class="">
+                <div class="m-6">
+                    <div class="rounded overflow-hidden shadow-lg">
+                        <img src="{{ $mobil->foto }}" alt="mobil" class="w-80 h-60 border-b-4 border-black ">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">{{ $mobil->nama_mobil }}</div>
+                            <p class="text-gray-700 text-base">{{ $mobil->nopol }}</p>
+                            <p class="text-gray-700 text-base">{{ $mobil->type }}</p>
+                            <p class="text-gray-700 text-base">{{ $mobil->warna }}</p>
+                            <p class="text-gray-700 text-base">{{ $mobil->sewa }}</p>
+                            <p class="text-gray-700 text-base">{{ $mobil->status }}</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
         </div>
-    </div>
-    @endforeach
-</body>
 
-</html>
+        @endforeach
+    </div>
+
+</section>
+@endsection
