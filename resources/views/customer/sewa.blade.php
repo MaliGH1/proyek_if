299c0804c328a5559d149a41bf09f6934c713082
MaliGH1 <rizkyfadjarmali@gmail.com>
2023-11-22 16:01:00 +0700

    commit

--- resources/views/customer/sewa.blade.php
+++ resources/views/customer/sewa.blade.php
@@ -54,12 +54,39 @@
                         <!-- Second Card -->
                         <div
                             class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
-                            <img class="rounded-t-sm lg:w-"
+                            <img class="rounded-t-sm  w-72 h-72 object-cover"
                                 src="https://i.pinimg.com/564x/cb/43/75/cb43750b56833308a168785d00e029d0.jpg" alt="" />
 
                             <div class="p-5">
                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
                                     Toyota Yaris
+                                </h5>
+
+                                <p class="mb-3 font-normal text-black-700 dark:text-black-400">
+                                    <strong style="font-size: 1.2em;">Rp. 300.000/hari</strong>
+                                </p>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Toyota Yaris</p>
+                                <a href="#"
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Sewa
+                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Third Card -->
+                    <div
+                            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
+                            <img class="rounded-t-sm   object-cover"
+                                src="https://i.pinimg.com/564x/91/46/62/914662cf3d64159e082cc2580df4358f.jpg" alt="" />
+
+                            <div class="p-5">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+                                    Mitsubishi Pajero
                                 </h5>
 
                                 <p class="mb-3 font-normal text-black-700 dark:text-black-400">
