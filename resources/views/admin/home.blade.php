63b12ce75365c60e4e41d8ffe669dea96e6faaf6
alexadma <alexadma16@gmail.com>
2023-11-29 20:40:16 +0700

    commit

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
     <div class="container flex left bg-black">
         <p class="text-white text-4xl">
         @if(auth()->check())
-                Selamat Datang {{ auth()->user()->username }}!
+                Selamat Datang !{{ auth()->user()->username }}!
             @else
                 Selamat Datang Admin
             @endif
