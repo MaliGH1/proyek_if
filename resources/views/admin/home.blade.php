86cc782684c7b0c734f182d4514e9510c29d4328
alexadma <alexadma16@gmail.com>
2023-11-29 20:46:25 +0700

    commit

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,6 +4,11 @@
 <section class="hero" id="home">
     <div class="container flex left bg-black">
         <p class="text-white text-4xl">
+        
+        <img src="image/DVJR.png" width=1000/>
+
+</svg>
+
         @if(auth()->check())
                 Selamat Datang !{{ auth()->user()->username }}!
             @else
--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}> 
-<img src="image/Logo DVJR.jpg" width=445/>
+<img src="image/DVJR.png" width=445/>
 
 </svg>
