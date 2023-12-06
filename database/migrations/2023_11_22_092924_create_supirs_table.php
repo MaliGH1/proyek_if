3f79b3ae80588d71610fa2caa77fb87861957f22
Ajitama38 <anjitama38@gmail.com>
2023-12-06 20:55:23 +0700

    commit

--- database/migrations/2023_11_22_092924_create_supirs_table.php
+++ database/migrations/2023_11_22_092924_create_supirs_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nama');
             $table->string('alamat');
             $table->string('nohpsupir');
-            $table->string('image')->nullable()->change();
+            $table->string('image');
             $table->timestamps();
         });
     }
