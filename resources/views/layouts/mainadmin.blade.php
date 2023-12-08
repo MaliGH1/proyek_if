984b14bdd9f1e70a16f453718c1992c454bcb5af
MaliGH1 <rizkyfadjarmali@gmail.com>
2023-12-08 11:31:12 +0700

    commit

--- resources/views/layouts/mainadmin.blade.php
+++ resources/views/layouts/mainadmin.blade.php
@@ -7,17 +7,20 @@
     <title>DVJR Rent Cars | HOME</title>
     <link rel="stylesheet" href="css/main.css" class="">
     <link href="/dist/tailwind.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     @vite('resources/css/app.css')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        .container {
-            width: 90%;
-            /* Atur lebar sesuai kebutuhan */
-            margin-left: auto;
-            /* Geser kontainer ke kanan */
-            margin-right: 0;
-            /* Pastikan margin kanan adalah 0 */
-        }
+    .container {
+        width: 90%;
+        /* Atur lebar sesuai kebutuhan */
+        margin-left: auto;
+        /* Geser kontainer ke kanan */
+        margin-right: 0;
+        /* Pastikan margin kanan adalah 0 */
+    }
     </style>
 </head>
 
@@ -28,7 +31,9 @@
     <div class="">
         @yield('container')
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
