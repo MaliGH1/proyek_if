c99dd32c721370dae32d78fd4c281e84df72dc29
MaliGH1 <rizkyfadjarmali@gmail.com>
2023-12-20 16:18:37 +0700

    commit

--- resources/views/mobil/tambahmobil.blade.php
+++ resources/views/mobil/tambahmobil.blade.php
@@ -62,7 +62,7 @@
             @enderror
         </div>
         <div class="mb-5">
-            <label for="tgl_pjk" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Pajak Mobil</label>
+            <label for="tgl_pjk" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Pajak Mobil (YYYY-MM-DD)</label>
             <input name="tgl_pjk" type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('tgl_pjk') is-invalid @enderror" required value="{{old('tgl_pjk')}}">
             @error('tgl_pjk')
                 <div class = "invalid-feedback">
