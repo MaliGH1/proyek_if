c02f35496bc4fa31e0607ac7e6f7168fb0c67813
MaliGH1 <rizkyfadjarmali@gmail.com>
2023-12-21 16:03:57 +0700

    commit

--- app/Http/Controllers/HomeAwal.php
+++ app/Http/Controllers/HomeAwal.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CustomerLoginController extends Controller
+class HomeAwal extends Controller
 {
     public function index()
     {
